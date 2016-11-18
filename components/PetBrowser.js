38e2edba7e43dfddd7b5b3da0191d301d6504a27
lshapz <laurashapiro01@gmail.com>
2016-11-18 12:24:18 -0500

    works but only in browser

--- components/PetBrowser.js
+++ components/PetBrowser.js
@@ -3,11 +3,13 @@ const React = require('react');
 const Pet = require('./Pet');
 
 class PetBrowser extends React.Component {
+
   render() {
+    
     const pets = this.props.pets.map((pet, index) => (
       <Pet pet={pet} key={index} onAdoptPet={this.props.onAdoptPet} isAdopted={this.props.adoptedPets.includes(pet.id)} />
     ));
-
+    
     return (
       <div className="ui cards">
         {pets}
